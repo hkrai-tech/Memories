3780489bc673150386262b09ae15232af6f9a2ae
hkrai-tech <8697422699hk@gmail.com>
2021-09-30 00:09:28 +0530

    Memories Readme

---
+++ README.md
@@ -0,0 +1,30 @@
+
+# MEMORIES
+
+A mern based app where a user can store some important memories related to him or others.
+
+
+
+
+## Tech Stack
+
+**Client:** React, 
+
+**Server:** Node, Express
+
+**DB:** MongoDB
+
+
+
+  
+## Features
+
+- User can create a memory with texts and photographs
+- user can Delete their memories
+- User can update their memories
+
+  
+## Screenshots
+
+![App Screenshot](https://res.cloudinary.com/cloud47/image/upload/v1632940579/WhatsApp_Image_2021-09-30_at_00.05.21_um8re9.jpg)
+  
--- server/index.js
+++ server/index.js
@@ -13,12 +13,14 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://coderhk47:12345-678910@cluster0.3bscv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const CONNECTION_URL = 'mongodb+srv://coderhk47:12345-678910@cluster0.3bscv.mongodb.net/samajik47?retryWrites=true&w=majority'
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
+
+
 
 //mongoose.set('useFindAndModify', false);
 
